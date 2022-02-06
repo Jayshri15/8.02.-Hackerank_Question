b67d1d33e3afca766ed2e86c5e7a31ca5b431ea3
Jayshri15 <83698383+Jayshri15@users.noreply.github.com>
2022-02-07 00:48:59 +0530

    Add files via upload

---
+++ without_split.py
@@ -0,0 +1,11 @@
+var = int(input("enter the chance"))
+numcount = 0
+num = []
+while numcount < var:
+    number=input("Enter a number: ")
+    numcount = numcount + 1
+    num.append(number)
+print(num)
+
+
+
